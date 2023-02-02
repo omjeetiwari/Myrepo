c08159b43d072ebf6356d5d01d65c53b2d6472cf
Omjee Vivekanand Tiwari <116361745+omjeetiwari@users.noreply.github.com>
2023-02-02 21:18:33 +0530

    Update matchstickgame.c
    play matchstic game

--- matchstickgame.c
+++ matchstickgame.c
@@ -2,25 +2,25 @@
 #include<conio.h>
 int main()
 {
-    int match_sticks = 21, user_choice, computer_choice;
-    while(match_sticks>=1)
+    int matchsticks = 21, uc, cc;
+    while(matchsticks>=1)
     {
-        printf("Total Match Sticks: %d\n", match_sticks);
+        printf("Total Match Sticks: %d\n", matchsticks);
         printf("Pick up the match sticks between (1 to 4): ");
-        scanf("%d", &user_choice);
+        scanf("%d", &uc);
 
-        if(user_choice>4)
+        if(uc>4)
         {
             printf("Invalid Entry");
             break;
         }
 
         
-        computer_choice = 6 - user_choice;
+        cc = 5 - uc;
         
-        printf("Computer picks up the %d match sticks.\n", computer_choice);
-        match_sticks = match_sticks-user_choice-computer_choice;
-        if(match_sticks==1)
+        printf("Computer picks up the %d match sticks.\n", cc);
+        matchsticks = matchsticks-uc-cc;
+        if(matchsticks==1)
         {
             printf("\nYou have been lost via computer.");
             break;
